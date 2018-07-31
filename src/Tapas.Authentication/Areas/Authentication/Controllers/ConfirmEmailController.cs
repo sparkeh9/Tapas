49c3bc8d1c0298f620196fa61018b7d0733b6607
Nick Briscoe <nick.briscoe@razor.co.uk>
2018-07-31 21:39:51 +0100

    Made registration and confirmation work

---
+++ src/Tapas.Authentication/Areas/Authentication/Controllers/ConfirmEmailController.cs
@@ -0,0 +1,52 @@
+﻿namespace Tapas.Authentication.Areas.Authentication.Controllers
+{
+    using System;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+    using Data.EntityFramework.Entities;
+    using Dto;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.UI.Services;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [ Area( "Authentication" ) ]
+    public class ConfirmEmailController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly ILogger<ConfirmEmailController> logger;
+        private readonly IEmailSender emailSender;
+
+        public ConfirmEmailController( ILogger<ConfirmEmailController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IEmailSender emailSender )
+        {
+            this.signInManager = signInManager;
+            this.logger = logger;
+            this.userManager = userManager;
+            this.emailSender = emailSender;
+        }
+
+        [ HttpGet ]
+        public async Task<IActionResult> Index( string userId, string code )
+        {
+            if ( userId == null || code == null )
+            {
+                return Redirect( Url.Content( "~/" ) );
+            }
+
+            var user = await userManager.FindByIdAsync( userId );
+            if ( user == null )
+            {
+                return NotFound( $"Unable to load user with ID '{userId}'." );
+            }
+
+            var result = await userManager.ConfirmEmailAsync( user, code );
+            if ( !result.Succeeded )
+            {
+                throw new InvalidOperationException( $"Error confirming email for user with ID '{userId}':" );
+            }
+
+            return View();
+        }
+    }
+}
--- src/Tapas.Authentication/Areas/Authentication/Controllers/RegisterController.cs
+++ src/Tapas.Authentication/Areas/Authentication/Controllers/RegisterController.cs
@@ -46,15 +46,12 @@ namespace Tapas.Authentication.Areas.Authentication.Controllers
                     logger.LogInformation("User created a new account with password.");
 
                     string code = await userManager.GenerateEmailConfirmationTokenAsync(user);
-                    string callbackUrl = Url.Page("/Account/ConfirmEmail",
-                                               pageHandler: null,
-                                               values: new { userId = user.Id, code = code },
-                                               protocol: Request.Scheme);
-
+                    string callbackUrl = Url.Action( "Index", "ConfirmEmail", new { userId = user.Id, code }, Request.Scheme );
+                    
                     await emailSender.SendEmailAsync(registerDto.Email, "Confirm your email",
                                                       $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    await signInManager.SignInAsync(user, isPersistent: false);
+                    await signInManager.SignInAsync(user, false);
                     return LocalRedirect(returnUrl);
                 }
                 foreach (var error in result.Errors)
