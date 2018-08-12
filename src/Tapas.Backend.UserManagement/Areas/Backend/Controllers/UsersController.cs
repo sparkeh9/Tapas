305abd7474756224e4e7ba506f47f5dd8b0b36a1
Nick Briscoe <nick.briscoe@razor.co.uk>
2018-08-12 22:06:25 +0100

    More user stuff

--- src/Tapas.Backend.UserManagement/Areas/Backend/Controllers/UsersController.cs
+++ src/Tapas.Backend.UserManagement/Areas/Backend/Controllers/UsersController.cs
@@ -1,15 +1,35 @@
 ﻿namespace Tapas.Backend.UserManagement.Areas.Backend.Controllers
 {
+    using System.Linq;
     using Core.Areas.Backend.Controllers;
+    using Data.EntityFramework.Entities;
+    using ExtCore.Data.Abstractions;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using ViewModels;
 
     [ Authorize( Policy = "Backend:Users:Manage" ) ]
     public class UsersController : BackendControllerBase
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        public UsersController(UserManager<ApplicationUser> userManager )
+        {
+            this.userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var users = userManager.Users.Select( u => new ListUsersViewModel.UserListing
+            {
+                Id = u.Id,
+                Username = u.UserName
+            } ).ToList();
+
+            return View(new ListUsersViewModel
+            {
+                Users = users
+            });
         }
     }
 }
---
+++ src/Tapas.Backend.UserManagement/Areas/Backend/ViewModels/ListUsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Tapas.Backend.UserManagement.Areas.Backend.ViewModels
+{
+    using System.Collections.Generic;
+
+    public class ListUsersViewModel
+    {
+        public List<UserListing> Users { get; set; }
+
+        public class UserListing
+        {
+            public string Username { get; set; }
+            public int Id { get; set; }
+        }
+    }
+}
